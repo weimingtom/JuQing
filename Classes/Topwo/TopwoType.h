1ada3c8a3b19e7c617574c06b5cd434143b8759d
topwo <topwo@foxmail.com>
2015-12-26 01:09:43 +0800

    恢复TopwoType

--- Classes/Topwo/TopwoType.h
+++ Classes/Topwo/TopwoType.h
@@ -4,22 +4,12 @@
 #include "TopwoDefine.h"
 #include "TopwoTools.h"
 USING_NS_CC;
-
 //打字类
-class TopwoType : public CCNode
+class TopwoType
 {
 public:
-	enum TypeLabelStyle{
-		LABEL_TTF,
-		LABEL_BMFONT
-	};
 	TopwoType();
 	virtual ~TopwoType();
-	virtual bool init() override;
-	CREATE_FUNC(TopwoType);
-
-	CCNode* createTypedLabel(TypeLabelStyle style, const char* fntfile, float fontSize);
-
 	//设置显示区域大小
 	void setTypeSize(CCSize size);
 	//设置打字间隔时间
@@ -29,26 +19,61 @@ public:
 
 	//设置打字的字符串
 	virtual void setTypeString(CCString *str);
-
 	//直接全部显示出来
-	virtual void typeAll();
+	virtual void typeAll() = 0;
 protected:
-	//第一帧对需要打印的字符串分析
-	virtual void updateOnce(float f);
 
-	//打字回调
-	virtual void typing(float f);
-
-	TypeLabelStyle __label_style;//类型
-	CCLabelProtocol* __label;//标签
 	CCSize __type_size;//内容区域大小
 	float __type_interval;//间隔时间
-	CCString* __type_str;//标签内容
+	CCString* __type_str;
 	twuint __type_char_sum;//打印的字符数
 	twuint __type_char_num;//打印的字符数
 	bool __is_type_all;//是否显示所有了
 
 	CCObject* __callbackListener;
 	SEL_CallFunc __callbackfunc;
+};
+class TopwoTypeBMFont : public CCLabelBMFont, public TopwoType
+{
+public:
+	TopwoTypeBMFont();
+	virtual ~TopwoTypeBMFont();
+
+	//创造函数
+	static TopwoTypeBMFont* create(const char* fntfile);
+
+	//初始函数
+	virtual bool init(const char* fntfile);
+
+	//设置打字的字符串
+	virtual void setTypeString(CCString *str);
+
+	//直接全部显示出来
+	virtual void typeAll();
+protected:
+	//打字回调
+	virtual void typing(float f);
+};
+
+class TopwoTypeTTF :public CCLabelTTF, public TopwoType
+{
+public:
+	TopwoTypeTTF();
+	virtual ~TopwoTypeTTF();
+
+	//创造函数
+	static TopwoTypeTTF* create(const char* fntfile, float fontSize);
+
+	//初始函数
+	virtual bool init(const char* fntfile, float fontSize);
+
+	//设置打字的字符串,t打字间隔时间
+	virtual void setTypeString(CCString *str);
+
+	//直接全部显示出来
+	virtual void typeAll();
+protected:
+	//打字回调
+	virtual void typing(float f);
 };
 #endif  //__TOPWO_TYPE_H__
